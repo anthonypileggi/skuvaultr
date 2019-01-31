e71e936277e572db1a1764ffcb53a2f4f9984827
anthonypileggi <anthony@ifixit.com>
2019-01-31 16:42:00 -0500

    remove old comments

--- R/sv_parse_response.R
+++ R/sv_parse_response.R
@@ -2,13 +2,6 @@
 #' @param response a nested list
 #' @export
 sv_parse_response <- function(response) {
-  # purrr::map_df(
-  #   response,
-  #   function(r) {
-  #     vars <- names(r)[purrr::map_lgl(r, ~length(.x) == 1)]   # TODO: this ignores entries with > 1 length (e.g., multiple suppliers)
-  #     tibble::as_tibble(r[vars])
-  #   }
-  # )
 
   purrr::map_df(
     response,
