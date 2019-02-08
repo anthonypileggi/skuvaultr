a3bfcc67fff111e595c6b75fb5a9bae979073508
anthonypileggi <anthony@ifixit.com>
2019-02-08 17:00:28 -0500

    add kit weights (optional)

--- R/sv_get_kits.R
+++ R/sv_get_kits.R
@@ -1,7 +1,7 @@
 #' Get kits
 #' @importFrom magrittr "%>%"
 #' @export
-sv_get_kits <- function(skus = NULL) {
+sv_get_kits <- function(skus = NULL, include_weights = TRUE) {
 
   products <-
     sv_api(
@@ -19,7 +19,7 @@ sv_get_kits <- function(skus = NULL) {
   }
 
   # parse response as tibble
-  purrr::map_df(
+  kits <- purrr::map_df(
     products,
     function(r) {
       vars <- names(r)[purrr::map_lgl(r, ~length(.x) == 1)]        # TODO: this ignores entries with > 1 length (e.g., multiple suppliers)
@@ -31,4 +31,42 @@ sv_get_kits <- function(skus = NULL) {
     }
   ) %>%
     dplyr::mutate_at(c("LastModifiedDateTimeUtc", "AvailableQuantityLastModifiedDateTimeUtc"), sv_parse_datetime)
+
+  # include weights (upon request)
+  if (include_weights)
+    kits <- sv_get_kit_weights(kits)
+
+  return(kits)
+}
+
+
+#' Get kit weights
+#' @param kits kit data from a call to \code{\link{sv_get_kits}}
+#' @importFrom magrittr "%>%"
+sv_get_kit_weights <- function(kits) {
+
+  # get SKUs contained within each kit
+  kit_items <- kits %>%
+    dplyr::select(Sku, KitLines) %>%
+    tidyr::unnest()
+
+  # get weights for base SKUs; sum weights over kit items
+  kit_weights <- kit_items %>%
+    dplyr::left_join(
+      sv_get_products(skus = unique(kit_items$SKU)),
+      by = c("SKU" = "Sku")
+    ) %>%
+    dplyr::group_by(Sku) %>%
+    dplyr::mutate(
+      weight = dplyr::case_when(
+        WeightUnit == "oz" ~ WeightValue / 16,
+        TRUE ~ WeightValue
+      )
+    ) %>%
+    summarize(
+      Weight = sum(Quantity * weight)
+    )
+
+  # join with original 'kits' data
+  dplyr::left_join(kits, kit_weights, by = "Sku")
 }
