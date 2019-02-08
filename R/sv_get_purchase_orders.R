40a967fff9a2377c04f0c2f7d5b3af633e001738
anthonypileggi <anthony@ifixit.com>
2019-02-08 17:00:52 -0500

    get purchase order data

---
+++ R/sv_get_purchase_orders.R
@@ -0,0 +1,12 @@
+#' Get purchase orders
+#' @importFrom magrittr "%>%"
+#' @export
+sv_get_purchase_orders <- function() {
+  x <- sv_api(path = "purchaseorders/getPOs")
+  x$PurchaseOrders %>%
+    sv_parse_response() %>%
+    dplyr::mutate_at(
+      c("CreatedDate", "OrderDate", "OrderCancelDate", "ArrivalDueDate", "RequestedShipDate", "ActualShippedDate"),
+      sv_parse_datetime
+      )
+}
