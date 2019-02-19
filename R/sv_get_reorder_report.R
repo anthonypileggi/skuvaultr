07ea9e76ab5c423def4f873206fa2b30b92b1a95
Anthony Pileggi <apileggi20@gmail.com>
2019-02-19 13:09:59 -0500

    update sv_get_reorder_report to include sales from last season

--- R/sv_get_reorder_report.R
+++ R/sv_get_reorder_report.R
@@ -3,15 +3,24 @@
 #' @param n_days Number of days in-stock to order
 #' @importFrom magrittr "%>%"
 #' @export
-sv_get_reorder_report <- function(n_days = 30) {
-  
+sv_get_reorder_report <- function(n_days = 30, include_last_season = TRUE) {
+
   # current stock
   products <- dplyr::filter(sv_get_products(), !IsAlternateSKU, !IsAlternateCode)
   kits <- sv_get_kits()
-  
+
   # recent sales data
-  sales <- sv_get_sales_details(start_date = Sys.Date() - 91, end_date = Sys.Date() - 1)
-  
+  start_date <- Sys.Date() - 119
+  end_date <- Sys.Date() - 1
+  last_year <- lubridate::year(end_date) - 1
+  season_start_date <- lubridate::as_date(paste0(last_year, "-02-20"))
+  season_end_date <- lubridate::as_date(paste0(last_year, "-07-31"))
+  days_LS <- as.numeric(difftime(season_end_date, season_start_date, units = "day")) + 1
+  if (include_last_season) {
+    start_date <- season_start_date
+  }
+  sales <- sv_get_sales_details(start_date = start_date, end_date = end_date)
+
   # get quantity sold for each sku over various date-ranges
   # - base skus
   product_df <- sales %>%
@@ -23,7 +32,9 @@ sv_get_reorder_report <- function(n_days = 30) {
       qty_7 = sum(Quantity[SaleDate >= Sys.Date() - 7]),
       qty_28 = sum(Quantity[SaleDate >= Sys.Date() - 28]),
       qty_63 = sum(Quantity[SaleDate >= Sys.Date() - 63]),
-      qty_91 = sum(Quantity[SaleDate >= Sys.Date() - 91])
+      qty_91 = sum(Quantity[SaleDate >= Sys.Date() - 91]),
+      qty_119 = sum(Quantity[SaleDate >= Sys.Date() - 119]),
+      qty_LS = sum(Quantity[SaleDate >= season_start_date & SaleDate <= season_end_date])
     )
   # - kit skus (string out kits to get qty counts for base skus)
   kit_df <- sales %>%
@@ -38,29 +49,37 @@ sv_get_reorder_report <- function(n_days = 30) {
       qty_7 = sum(Quantity[SaleDate >= Sys.Date() - 7]),
       qty_28 = sum(Quantity[SaleDate >= Sys.Date() - 28]),
       qty_63 = sum(Quantity[SaleDate >= Sys.Date() - 63]),
-      qty_91 = sum(Quantity[SaleDate >= Sys.Date() - 91])
+      qty_91 = sum(Quantity[SaleDate >= Sys.Date() - 91]),
+      qty_119 = sum(Quantity[SaleDate >= Sys.Date() - 119]),
+      qty_LS = sum(Quantity[SaleDate >= season_start_date & SaleDate <= season_end_date])
     ) %>%
     dplyr::rename(Sku = SKU)
   # - combine base/kit skus
   df <- dplyr::bind_rows(product_df, kit_df) %>%
     dplyr::group_by(Sku) %>%
     dplyr::summarize_if(is.numeric, sum)
-  
+
   # compute days_left and suggested_qty
   # -- based on sales in past 28 days
   products %>%
     dplyr::filter(
       !stringr::str_detect(Statuses, "NLA|Inactive")
     ) %>%
-    dplyr::select(Sku, Code, Description, Brand, Supplier, Cost, QuantityAvailable, QuantityIncoming, QuantityTotalFBA) %>%
+    dplyr::mutate(
+      Created = lubridate::date(CreatedDateUtc)
+    ) %>%
+    dplyr::select(Sku, Code, Description, Brand, Supplier, Created, Cost, QuantityAvailable, QuantityIncoming, QuantityTotalFBA) %>%
     dplyr::left_join(df, by = "Sku") %>%
     tidyr::replace_na(
-      list(qty_7 = 0, qty_28 = 0, qty_63 = 0, qty_91 = 0)
+      list(qty_7 = 0, qty_28 = 0, qty_63 = 0, qty_91 = 0, qty_119 = 0, qty_LS = 0)
     ) %>%
     dplyr::mutate(
       days_left = ifelse(qty_28 == 0, Inf, QuantityAvailable / (qty_28 / 28)),
+      days_left = round(days_left, 1),
       suggested_qty = (qty_28 / 28) * n_days - QuantityAvailable - QuantityIncoming,
-      suggested_qty = ifelse(suggested_qty < 0, 0, ceiling(suggested_qty))
+      suggested_qty = ifelse(suggested_qty < 0, 0, ceiling(suggested_qty)),
+      suggested_qty_LS = (qty_LS / days_LS) * n_days - QuantityAvailable - QuantityIncoming,
+      suggested_qty_LS = ifelse(suggested_qty_LS < 0, 0, ceiling(suggested_qty_LS))
     ) %>%
     dplyr::arrange(desc(suggested_qty))
 }
