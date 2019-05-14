fe5ca8ee5b09c66720377d137707d17d5e8c121b
anthonypileggi <anthony@ifixit.com>
2019-05-14 16:25:26 -0400

    convert order to a picklist

---
+++ R/sv_order_as_picklist.R
@@ -0,0 +1,26 @@
+
+#' Convert an order to a picklist
+#' @param saleid SaleId of order to pick
+#' @importFrom magrittr "%>%"
+#' @export
+sv_order_as_picklist <- function(saleid) {
+
+  # pull up order information
+  order <- skuvaultr::sv_get_sales(order_id = saleid)
+  if (nrow(order) != 1)
+    stop(paste("Order", saleid, "not found!"))
+
+  # get order contents
+  picklist <- dplyr::bind_rows(
+    purrr::map_df(order$SaleKits, ~.x$Items),
+    purrr::map_df(order$SaleItems, ~dplyr::select(.x, Sku, Quantity))
+  )
+
+  # get item locations
+  locs <- skuvaultr::sv_get_inventory_locations(skus = picklist$Sku) %>%
+    dplyr::mutate_at("Sku", toupper) %>%
+    dplyr::rename(Available = Quantity)
+  picklist <- dplyr::left_join(picklist, locs, by = "Sku")
+
+  return(list(status = order$Status, picklist = picklist))
+}
