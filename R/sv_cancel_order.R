488c3551d3e4ec50b0803d9189e4d25589243782
anthonypileggi <anthony@ifixit.com>
2019-05-14 16:25:33 -0400

    cancel sales and return inventory to warehouse

---
+++ R/sv_cancel_order.R
@@ -0,0 +1,35 @@
+#' Cancel an order (and auto-unpick items)
+#' @param saleid SaleId of order to pick
+#' @importFrom magrittr "%>%"
+#' @export
+sv_cancel_order <- function(saleid) {
+
+  # load order picklist
+  order <- sv_order_as_picklist(saleid)
+  picklist <- order$picklist
+
+  # checks
+  if (order$status == "Pending")
+    stop("Order has not been picked!")
+  if (order$status == "Cancelled")
+    stop("Order is already cancelled!")
+  if (any(is.na(picklist$LocationCode)))
+    stop("Cannot unpick order due to out-of-stock")
+
+  # add inventory back into warehouse (if already picked)
+  if (order$status %in% c("ReadyToShip", "Completed")) {
+    out <- picklist %>%
+      dplyr::mutate(
+        WarehouseId = 2640,
+        Reason = "Cancelled"
+      ) %>%
+      dplyr::select(Sku, Quantity, WarehouseId, LocationCode, Reason)
+    sv_api_call(
+      path = "inventory/addItemBulk",
+      Items = out
+    )
+  }
+
+  # cancel order
+  sv_update_sale_status(saleid, "Cancelled")
+}
