b3f72af556ddb3c2811b349143d179848cb1497a
anthonypileggi <anthony@ifixit.com>
2019-05-14 16:25:54 -0400

    use sv_order_as_picklist

--- R/sv_pick_order.R
+++ R/sv_pick_order.R
@@ -4,28 +4,17 @@
 #' @importFrom magrittr "%>%"
 #' @export
 sv_pick_order <- function(saleid) {
-  
-  # pull up order information
-  order <- sv_get_sales(order_id = saleid)
-  if (nrow(order) != 1)
-    stop(paste("Order", saleid, "not found!"))
-  
-  # get order contents
-  picklist <- dplyr::bind_rows(
-    purrr::map_df(order$SaleKits, ~.x$Items),
-    purrr::map_df(order$SaleItems, ~dplyr::select(.x, Sku, Quantity))
-  )
-  
-  # get item locations
-  locs <- skuvaultr::sv_get_inventory_locations(skus = picklist$Sku) %>%
-    dplyr::mutate_at("Sku", toupper) %>%
-    dplyr::rename(Available = Quantity)
-  picklist <- dplyr::left_join(picklist, locs, by = "Sku")
-  
-  # check inventory levels
+
+  # load order picklist
+  order <- sv_order_as_picklist(saleid)
+  picklist <- order$picklist
+
+  # checks
+  if (order$status != "Pending")
+    stop("Order does not need to be picked!")
   if (any(picklist$Quantity > picklist$Available))
     stop("Not enough inventory to pick this order!")
-  
+
   # prepare picklist
   picklist <- picklist %>%
     dplyr::transmute(
@@ -37,13 +26,13 @@ sv_pick_order <- function(saleid) {
       IsExpressPick = FALSE,
       Note = "Picked using API"
     )
-  
+
   # pick order
   sv_api_call(
     path = "inventory/pickItemBulk",
     Items = picklist
   )
-  
+
   # complete order after picking
   sv_update_sale_status(saleid, "Complete")
 }
