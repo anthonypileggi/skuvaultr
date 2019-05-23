210113647477313d2af7411e0c2d221c5ba13f1c
anthonypileggi <anthony@ifixit.com>
2019-05-23 15:42:30 -0400

    fix picklist for kits too

--- R/sv_order_as_picklist.R
+++ R/sv_order_as_picklist.R
@@ -11,10 +11,11 @@ sv_order_as_picklist <- function(saleid) {
     stop(paste("Order", saleid, "not found!"))
 
   # get order contents
-  picklist <- dplyr::bind_rows(
-    purrr::map_df(order$SaleKits, ~.x$Items[[1]]),
-    purrr::map_df(order$SaleItems, ~dplyr::select(.x, Sku, Quantity))
-  )
+  picklist <- NULL
+  if (nrow(order$SaleKits[[1]]) > 0)
+    picklist <- dplyr::bind_rows(picklist, purrr::map_df(order$SaleKits, ~.x$Items[[1]]))
+  if (nrow(order$SaleItems[[1]]) > 0)
+    picklist <- dplyr::bind_rows(picklist, purrr::map_df(order$SaleItems, ~dplyr::select(.x, Sku, Quantity)))
 
   # get item locations
   locs <- skuvaultr::sv_get_inventory_locations(skus = picklist$Sku) %>%
