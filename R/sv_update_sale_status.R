f001161762eb162cfd27274dc3052ba01c5f019e
anthonypileggi <anthony@ifixit.com>
2019-05-24 14:44:28 -0400

    accept a vector of saleids

--- R/sv_update_sale_status.R
+++ R/sv_update_sale_status.R
@@ -1,15 +1,25 @@
 
 #' Update the status of a sale
-#' @param saleid saleID of the order
-#' @param status order status to set
+#' @param saleid saleID of the order (character/vector)
+#' @param status order status to set (character/scalar)
 #' @export
-sv_update_sale_status <- function(saleid, status = c("Completed", "Cancelled", "Pending", "ReadyToShip", "Invalid", "ShippedUnpaid")) {
-  
+sv_update_sale_status <- function(saleid,
+                                  status = c("Completed", "Cancelled", "Pending", "ReadyToShip", "Invalid", "ShippedUnpaid")) {
+
   status <- match.arg(status)
-  
-  skuvaultr::sv_api_call(
-    path = "sales/updateOnlineSaleStatus",
-    SaleId = saleid,
-    Status = status
+
+  purrr::map(
+    seq_along(saleid),
+    function(i) {
+      if (i > 1)
+        Sys.sleep(4)
+      skuvaultr::sv_api_call(
+        path = "sales/updateOnlineSaleStatus",
+        SaleId = saleid[i],
+        Status = status
+      )
+      message(paste0("Updated status for SaleId ", saleid[i], " to '", status, "'."))
+    }
   )
+
 }
