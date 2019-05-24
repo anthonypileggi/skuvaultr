a57fe695b2c8a9f4de73d70b3c01d5d32bd75fbf
anthonypileggi <anthony@ifixit.com>
2019-05-24 14:43:55 -0400

    take a (multi-order) picklist as an argument

--- R/sv_pick_order.R
+++ R/sv_pick_order.R
@@ -1,24 +1,35 @@
 
 #' Auto-Pick an order
-#' @param saleid SaleId of order to pick
+#' @param saleid SaleId of order to pick (character/scalar)
+#' @param picklist prepared picklist (data.frame)
 #' @importFrom magrittr "%>%"
 #' @export
-sv_pick_order <- function(saleid) {
+sv_pick_order <- function(saleid = NULL, picklist = NULL) {
 
-  # load order picklist
-  order <- sv_order_as_picklist(saleid)
-  picklist <- order$picklist
+  if (is.null(saleid) & is.null(picklist))
+    stop("Must provide a `saleid` or a prepared `picklist`!")
 
-  # checks
-  if (order$status %in% c("Cancelled", "Completed"))
-    stop("Order does not need to be picked!")
+  # load order picklist (if not provided)
+  if (is.null(picklist)) {
+    order <- sv_order_as_picklist(saleid)
+    picklist <- order$picklist
+    picklist$Id <- saleid
+
+    # check order status
+    if (order$status %in% c("Cancelled", "Completed"))
+      stop("Order does not need to be picked!")
+  }
+
+  # check picklist format
   if (any(picklist$Quantity > picklist$Available))
     stop("Not enough inventory to pick this order!")
+  if (!all(c("Id", "Sku", "Quantity", "LocationCode", "Available") %in% names(picklist)))
+    stop("Picklist does not have required fields!")
 
   # prepare picklist
   picklist <- picklist %>%
     dplyr::transmute(
-      SaleId = saleid,
+      SaleId = Id,
       Sku = Sku,
       WarehouseId = 2640,    # WH1
       LocationCode = LocationCode,
@@ -33,7 +44,7 @@ sv_pick_order <- function(saleid) {
     Items = picklist
   )
 
-  # complete order after picking
+  # complete order(s) after picking
   Sys.sleep(5)
-  sv_update_sale_status(saleid, "Complete")
+  sv_update_sale_status(unique(picklist$SaleId), "Complete")
 }
