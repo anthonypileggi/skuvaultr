7e2e550811dd66eaec7c6eb362c4b5cd26cfb81a
anthonypileggi <anthony@ifixit.com>
2019-06-05 15:14:13 -0400

    use internal parsing function

--- R/sv_get_orders.R
+++ R/sv_get_orders.R
@@ -6,35 +6,15 @@
 sv_get_orders <- function(start_date = Sys.Date() - 1, end_date = Sys.Date()) {
 
   # get all stens dropship-eligble orders
-  sales <- skuvaultr::sv_get_sales(start_date = start_date, end_date = end_date)
+  sales <- sv_get_sales(start_date = start_date, end_date = end_date)
 
   # extract order components
-  v <- c("FulfilledKits", "MerchantKits", "FulfilledItems", "MerchantItems", "SaleKits", "SaleItems")
-  v <- intersect(v, names(sales))
-  out <- purrr::map_df(v, ~tidyr::unnest(dplyr::select(sales, Id, .x)))
-
-  # TODO: attach actual 'Order' contents (i.e., w/ kits)
-  # TODO: return all fields, not just a select few...
-
-  # unnest kits
-  if ("Items" %in% names(out)) {
-    out <- dplyr::mutate(out, is_kit = !purrr::map_lgl(Items, is.null))
-    kits <- out %>%
-      dplyr::filter(is_kit) %>%
-      tidyr::unnest() %>%
-      dplyr::select(Id, Sku = Sku1, Quantity = Quantity1, PartNumber = PartNumber1)
-    parts <- out %>%
-      dplyr::filter(!is_kit) %>%
-      dplyr::select(Id, Sku, Quantity, PartNumber)
-    out <- dplyr::bind_rows(kits, parts) %>%
-      dplyr::arrange(Id)
-  } else {
-    out <- dplyr::select(out, Id, Sku, Quantity, PartNumber)
-  }
+  out <- sv_parse_order_contents(sales)
 
   # attach item classifications
   out <- out %>%
-    dplyr::mutate_at(c("Sku", "PartNumber"), toupper) %>%
+    tidyr::unnest() %>%
+    dplyr::mutate_at("Sku", toupper) %>%
     dplyr::left_join(
       skuvaultr::sv_get_products(skus = unique(.$Sku)) %>%
         dplyr::select(Sku, Classification, Supplier, QuantityAvailable),
