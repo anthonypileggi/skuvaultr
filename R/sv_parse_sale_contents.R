eff53c13183f8628f124ea66f269a735a956afd5
anthonypileggi <apileggi20@gmail.com>
2020-05-14 19:47:37 -0400

    robustify; return total sale value

--- R/sv_parse_sale_contents.R
+++ R/sv_parse_sale_contents.R
@@ -3,18 +3,18 @@
 #' @importFrom magrittr "%>%"
 #' @export
 sv_parse_sale_contents <- function(x) {
-  
+
   v <- c("FulfilledKits", "MerchantKits", "FulfilledItems", "MerchantItems", "SaleItems", "SaleKits")
   v <- intersect(v, names(x))
-  
+
   # extract sale items
   out <-
     purrr::map_df(
       v,
-      function(v) {
-        out <- sales %>%
-          dplyr::select(Id, v) %>%
-          tidyr::unnest()
+      function(vv) {
+        out <- x %>%
+          dplyr::select(Id, vv) %>%
+          tidyr::unnest(cols = c(vv))
         if ("Price" %in% names(out))
           out <- dplyr::mutate(out, Price = as.numeric(stringr::str_sub(Price, 2, -1)))
         if ("Items" %in% names(out))
@@ -22,12 +22,20 @@ sv_parse_sale_contents <- function(x) {
         out
       }
     )
-  
-  # uppercase
-  out <- dplyr::mutate_at(out, "Sku", toupper)
-  
-  # nest (i.e., 1 row per SaleId)
-  out <- tidyr::nest(out, -Id, .key = "Items")
-  
+
+  if (!("PartNumber" %in% names(out)))
+    out$PartNumber <- NA_character_
+
+  out <- out %>%
+    dplyr::mutate_at("Sku", toupper) %>%                                # uppercase SKU
+    tidyr::nest(Items = c(Sku, PartNumber, Quantity, Price)) %>%         # 1 row per SaleId
+    dplyr::mutate(
+      Total = purrr::map_dbl(Items, ~sum(.x$Quantity * .x$Price))       # order total
+    ) %>%
+    dplyr::left_join(
+      dplyr::transmute(x, Id, ShippingCost = as.numeric(stringr::str_sub(ShippingCost, 2, -1))),
+      by = "Id"
+    )
+
   return(out)
 }
