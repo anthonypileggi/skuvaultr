c8639a1bbb70a56eed119148dee3e103c3065f93
anthonypileggi <apileggi20@gmail.com>
2020-05-14 19:09:28 -0400

    parse sales channel from Marketplace/fulfillments

---
+++ R/sv_parse_channel.R
@@ -0,0 +1,23 @@
+
+#' Parse Sales Channel from sales data
+#' @param x sales data from a call to \code{\link{sv_get_sales}}
+#' @importFrom magrittr "%>%"
+#' @export
+sv_parse_channel <- function(x) {
+  
+  x %>%
+    dplyr::mutate(
+      is_fulfilled = purrr::map2_lgl(FulfilledItems, FulfilledKits, ~nrow(.x) > 0 | nrow(.y) > 0),
+      Channel = dplyr::case_when(
+        Marketplace == "Amazon Seller Central - US" & is_fulfilled  ~ "Amazon FBA",
+        Marketplace == "Amazon Seller Central - US" & !is_fulfilled ~ "Amazon",
+        Marketplace == "eBay Fixed Price"                           ~ "eBay",
+        Marketplace == "Unknown"                                    ~ "Pricefalls",
+        Marketplace == "Manual"                                     ~ "Service/Returns",
+        Marketplace == "Walmart Marketplace"                        ~ "Walmart",
+        TRUE                                                        ~ Marketplace
+      )
+    ) %>%
+    dplyr::select(Id, Channel)
+  
+}
