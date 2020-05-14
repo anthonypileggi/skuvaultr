04d307cb3ca0fb6fa6a89cb331834165fbe26c23
anthonypileggi <apileggi20@gmail.com>
2020-05-14 19:09:10 -0400

    return all useful information within sv_get_orders

--- R/sv_get_orders.R
+++ R/sv_get_orders.R
@@ -6,43 +6,52 @@
 #' @export
 sv_get_orders <- function(start_date = Sys.Date() - 1, end_date = Sys.Date(), order_id = NULL) {
 
-  # get all stens dropship-eligble orders
+  # load orders w/in date range
   sales <- sv_get_sales(start_date = start_date, end_date = end_date, order_id = order_id)
 
-  # extract order components
-  out <- sv_parse_order_contents(sales)
-
-  # attach item classifications
-  out <- out %>%
-    tidyr::unnest(Items) %>%
-    dplyr::mutate_at("Sku", toupper) %>%
+  # attach order details (date/marketplace/recipient/shipping-details)
+  out <- sales %>%
+    dplyr::select(-Channel) %>%
     dplyr::left_join(
-      skuvaultr::sv_get_products(skus = unique(.$Sku)) %>%
-        dplyr::select(Sku, Classification, Supplier, QuantityAvailable),
-      by = "Sku"
-    )
-
-  # classify orders
-  order_class <- out %>%
-    dplyr::group_by(Id) %>%
-    dplyr::summarize_at(
-      c("Classification", "Supplier"),
-      function(y) paste(unique(y), collapse = "; ")
-    )
-
-  # get contact/shipping details
-  order_ship <- sales %>%
-    dplyr::select(Id, Status, SaleDate, Marketplace, ShippingClass, PrintedStatus, Client, ShippingInfo, ContactInfo) %>%
-    tidyr::unnest(c(ShippingInfo, ContactInfo))
-
-  # attach order class and additional details
-  out <- out %>%
-    tidyr::nest(Items = c(Sku, Quantity, Classification, Supplier, QuantityAvailable)) %>%
-    dplyr::mutate(
-      Items = purrr::map(Items, ~.x)
+      sv_parse_channel(sales),
+      by = "Id"
     ) %>%
-    dplyr::left_join(order_class, by = "Id") %>%
-    dplyr::left_join(order_ship, by = "Id")
+    dplyr::select(Id, SaleDate, Channel, ShippingClass, Printed = PrintedStatus, ShippingInfo, ContactInfo)
+
+  # extract order components (unnest kits)
+  out <- out %>%
+    dplyr::left_join(
+      sv_parse_order_contents(sales),
+      by = "Id"
+    )
+
+  # get picklist and fulfillment type
+  out <- out %>%
+    dplyr::left_join(
+      sv_get_picklists(data = sales),
+      by = "Id"
+    ) %>%
+    dplyr::rename(Fulfill = fulfill, Picklist = picklist)
+
+  # sale summaries
+  out <- out %>%
+    dplyr::left_join(
+      sv_parse_sale_contents(sales) %>%
+        dplyr::select(-Items),
+      by = "Id"
+    )
+
+  # attach product supplier/cost/classification
+  skus <- unique(unlist(purrr::map(out$Items, ~.x$Sku)))
+  products <- skuvaultr::sv_get_products(skus = skus) %>%
+    dplyr::select(Sku, Classification, Supplier, Cost, QuantityAvailable)
+  out <- out %>%
+    dplyr::mutate(
+      Items = purrr::map(Items, ~dplyr::left_join(.x, products, by = "Sku")),
+      Classification = purrr::map_chr(Items, ~paste(unique(.x$Classification), collapse = "; ")),
+      Supplier = purrr::map_chr(Items, ~paste(unique(.x$Supplier), collapse = "; ")),
+      Cost = purrr::map_dbl(Items, ~sum(.x$Cost * .x$Quantity))
+    )
 
   return(out)
 }
