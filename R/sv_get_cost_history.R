544006481087cf65701b65efbc26ca000a8712c9
anthonypileggi <apileggi20@gmail.com>
2020-05-28 18:42:53 -0400

    update cost-history function

--- R/sv_get_cost_history.R
+++ R/sv_get_cost_history.R
@@ -1,29 +1,35 @@
 
 #' Construct cost history
-#' @param skus skus (character/vector)
 #' @importFrom magrittr "%>%"
 #' @export
-sv_get_cost_history <- function(skus = "hon1010") {
-  
+sv_get_cost_history <- function() {
+
   purchases <- dplyr::bind_rows(
     skuvaultr::sv_get_purchase_orders(),
     skuvaultr::sv_get_purchase_orders(Status = "Completed")
   ) %>%
     dplyr::filter(Status != "Cancelled")
-  
-  purchases %>%
-    tidyr::unnest() %>%
-    dplyr::filter(toupper(SKU) %in% toupper(skus)) %>%
-    dplyr::select(OrderDate, SKU, Quantity, ReceivedQuantity, Cost) %>%
+
+  df <- purchases %>%
+    dplyr::select(PoNumber, CreatedDate, LineItems) %>%
+    tidyr::unnest(cols = c(LineItems)) %>%
+    #dplyr::filter(toupper(SKU) %in% toupper(skus)) %>%
+    dplyr::select(Date = CreatedDate, Sku = SKU, Quantity, ReceivedQuantity, Cost) %>%
     dplyr::filter(Cost > 0) %>%
     dplyr::mutate(
-      Date = lubridate::as_date(OrderDate)
+      Date = lubridate::as_date(Date)
     ) %>%
-    dplyr::group_by(SKU) %>%
-    tidyr::nest(.key = "history") %>%
+    dplyr::arrange(Date) %>%
+    tidyr::nest(history = c(Date, Quantity, ReceivedQuantity, Cost))
+
+  p <- dplyr::progress_estimated(length(unique(df$Sku)))
+
+  df %>%
     dplyr::mutate(
       cost_history = purrr::map(history,
         function(x) {
+          p$tick()$print()
+          #cat(s, "\n")
           x %>%
             dplyr::full_join(
               dplyr::tibble(Date = seq(min(x$Date), max(x$Date), by = "day")),
@@ -33,7 +39,6 @@ sv_get_cost_history <- function(skus = "hon1010") {
             tidyr::fill(Cost, .direction = "down") %>%
             dplyr::select(Date, Cost)
         })
-    ) %>%
-    dplyr::select(sku = SKU, cost_history)
+    )
 }
 
